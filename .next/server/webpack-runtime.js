37227b35695bb5fa089c27caab28a2dfc25a5b3e
junipercabinetry <junipercabinetry@gmail.com>
2026-01-29 09:51:46 -0800

    Updated globals.css

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1b1d0861639116bf")
+/******/ 		__webpack_require__.h = () => ("a4210df9b74d4f2f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
