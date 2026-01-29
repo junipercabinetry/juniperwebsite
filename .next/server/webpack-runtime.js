9344297667418001df2352a7c533458687a1f793
junipercabinetry <junipercabinetry@gmail.com>
2026-01-29 09:46:34 -0800

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("d85a43aafc561ece")
+/******/ 		__webpack_require__.h = () => ("f018df28a906e4ae")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cedda877937eedba"; }
+/******/ 		__webpack_require__.h = function() { return "e7489c844ae4aa9b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
